3335d8fe6e63a706cbf3e105d2750a5f86bf83e3
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-02-27 16:39:28 -0500

    worked on navbar css

--- frontend_interest/src/components/DisplayOnePinById.js
+++ frontend_interest/src/components/DisplayOnePinById.js
@@ -1,28 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import NavBarContainer from './NavBarContainer.js'
+import '../css/DisplayOnePinById.css'
 
-
-class DisplayOnePinById extends Component{
-
-
-
-componentDidMount(){
-  this.props.fetchOnePin(this.props.match.params.id)
-}
-
-  render(){
-    // console.log(this.props.match.params.id, "the id")
-    // console.log(this.props, "the pin id");
-    if(!this.props.pin) return null
-    return(
-      <div className="displayOnePinPage">
-
-        hello world
-        <img src={this.props.pin.imgurl} alt="" />
-      </div>
-    )
+class DisplayOnePinById extends Component {
+  componentDidMount() {
+    this.props.fetchOnePin(this.props.match.params.id);
   }
 
+  render() {
+    // console.log(this.props.match.params.id, "the id")
+    // console.log(this.props, "the pin id");
+    if (!this.props.pin) return null;
+    return (
+      <div className="displayOnePinPage">
+        <NavBarContainer />
+        <div className="mainImageDiv">
+        <div className="imageContainer">
+          <img className="onePinImage" src={this.props.pin.imgurl} alt="" />
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
 
-
-export default DisplayOnePinById
+export default DisplayOnePinById;
--- frontend_interest/src/components/NavBar.js
+++ frontend_interest/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../frontendResources/InterestHeartlogo.png";
 import "../css/NavBar.css";
-import {withRouter} from 'react-router-dom';
+import {withRouter, Link} from 'react-router-dom';
 import magnifyingGlass from '../frontendResources/magnifying_glass.png'
 
 
@@ -20,45 +20,46 @@ class NavBar extends React.Component{
     this.props.logoutUser()
   }
 
-//   displayUserWithoutEmail = () => {
-// return this.props.currentUser.substring(0, this.props.currentUser.lastIndexOf("@"))
-//
-// //     var email = this.props.currentUser
-// // var name = email.substring(0, email.lastIndexOf("@"));
-// // console.log(name, "the name<3")
-// //   return name;
-//
-//   }
+  displayUserWithoutEmail = () => {
+return window.localStorage.token.substring(0, window.localStorage.token.lastIndexOf("@"))
 
-displayCurrentUser = () => {
-  if(this.props.currentUser){
-    return(
+//     var email = this.props.currentUser
+// var name = email.substring(0, email.lastIndexOf("@"));
+// console.log(name, "the name<3")
+//   return name;
 
-    <li className="list">{this.props.currentUser.email}</li>
-    )
-  }else{
-    return null
   }
-}
+//
+// displayCurrentUser = () => {
+//   if(this.props.currentUser){
+//     return(
+//
+//     <li className="list">{this.props.currentUser.email}</li>
+//     )
+//   }else{
+//     return null
+//   }
+// }
 
 
 render(){
-
+  // let {currentUser} = this.props;
   // console.log(this.props.currentUser, "navbar");
   // if(this.props.currentUser){
+  // if(!currentUser) return null
   return (
     <div className="navBarPage">
       <div className="logoAndSearch">
-        <img className="logo" src={logo} alt="" />
+        <Link className="logoHome" to={"/Home"}><img className="logo" src={logo} alt="" /></Link>
         <div className="searchBoxAndMagnifyingStyling">
         <img className="logo" src={magnifyingGlass} alt="" />
         <input className="searchBox" type="text" placeholder="search" />
         </div>
         <div className="listItems">
           <ul>
-            <li className="list">Home</li>
-            {this.displayCurrentUser()}
-            <li className="list"><button onClick={this.handleLogout}>log out</button></li>
+            <li className="list"> <Link className="naviLink"to={"/Home"}> Home</Link></li>
+            <li className="list">{this.displayUserWithoutEmail()}</li>
+            <li onClick={this.handleLogout } className="list" >log out</li>
 
           </ul>
         </div>
