32f8f75caeb01e31b1fb8380e35b02324fcea761
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-06-23 22:05:52 -0400

    hv3

--- backend_interest/app.js
+++ backend_interest/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "frontend_interest/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/frontend_interest/build/index.html"));
+  res.sendFile(path.join(__dirname + "../frontend_interest/build/index.html"));
 });
 
 app.use(
